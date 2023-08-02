07477c368f731656ee9af079f59c5256f59d2671
hibo <hiroaki.sato.031030@gmail.com>
2023-08-02 16:52:39 +0900

    スライダー

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,37 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export default function Home() {
+export default function SimpleSlider() {
+  const settings = {
+    dots: true, //スライダー下の点の表示/非表示
+    infinite: true, //無限スクロールの有効/無効
+    speed: 500, //スライド切り替えの速度
+    slidesToShow: 1, //一度に表示するスライド数
+    slidesToScroll: 1, //一度にスライドする枚数
+  };
+
   return (
-    <div>
-      <h1>TOP画面</h1>
-      <h2>TOP画面</h2>
-    </div>
+    <Slider {...settings}>
+      <div>
+        <h3>Slide 1</h3>
+      </div>
+      <div>
+        <h3>Slide 2</h3>
+      </div>
+      <div>
+        <h3>Slide 3</h3>
+      </div>
+      <div>
+        <h3>Slide 4</h3>
+      </div>
+      <div>
+        <h3>Slide 5</h3>
+      </div>
+    </Slider>
   );
 }
